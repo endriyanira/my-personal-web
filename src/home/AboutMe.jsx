6831fbe52c85b44122c4d438cb17aecbe9aaa0b4
Endriyani Rahayu <endriyanirahayu@192.168.40.40>
2024-07-08 16:48:26 +0700

    added about me on home page

---
+++ src/home/AboutMe.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section className="aboutme-section content-start items-start gap-8 flex flex-col basis-auto flex-nowrap justify-start overflow-x-hidden overflow-y-hidden py-0 px-5 md:px-8 xl:px-[50px] relative">
+      <div className="section-title flex basis-auto flex-col gap-3 justify-start relative text-wrap md:leading-7 xl:leading-8">
+        <h1 className="block font-syne text-2xl md:text-[43px] font-bold leading-normal text-left">
+          About Me
+        </h1>
+        <h2 className="font-syne font-normal text-secondaryHeadingText dark:text-secondaryDarkText text-[23px]">
+          Who Am I?
+        </h2>
+        <h4 className="text-primaryLightText dark:text-primaryDarkText text-lg md:text-xl xl:text-[22px] md:leading-7 xl:leading-8 font-inter font-light">
+          I am a fresh graduate with a keen interest in frontend development.
+          Throughout my academic journey, I have honed my skills in HTML, CSS,
+          JavaScript, and various frontend frameworks. My goal is to create
+          visually appealing and user-friendly websites.
+        </h4>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import AboutMe from "./AboutMe";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home content-center items-center gap-[106px] flex flex-col flex-nowrap justify-start overflow-y-hidden overflow-x-hidden p-0 relative">
       <Hero />
+      <AboutMe />
     </div>
   );
 };
